4280127fa7d4cff6189cfef56c768040cc3433b5
terraform-master <72972937+terraform-master@users.noreply.github.com>
2020-11-24 14:23:42 +0100

    Create azure-vnet.tf

---
+++ azure-vnet.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  version = "~> 2.1"
+}
+
+resource "azurerm_resource_group" "rg" {
+    name            = var.rgname
+    location        = var.location
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnetname
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["172.16.0.0/16"]
+}
